5c9ea59fd039b5fbe2efebea92c6ae0963870d5e
Victoria Johnston <victoria13johnston@gmail.com>
2020-05-30 13:00:49 -0400

    Add files via upload

---
+++ Camera&Display/Camera&Display/AddPhotoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddPhotoViewController.swift
+//  Camera&Display
+//
+//  Created by gwc on 7/29/19.
+//  Copyright © 2019 Kode With Klossy 2019. All rights reserved.
+//
+
+import UIKit
+
+class AddPhotoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
